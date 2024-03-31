2c4bf3dbf39cb13e43256c2862baa2f1ab46f3b6
Osei-Nathan <nohenebeng001@st.ug.edu.gh>
2024-03-31 09:55:04 +0000

    changed input board

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ def display_board(board):
         if board[i] == 0:
             print("*", end=" ")
         elif board[i] == 1:
-            print("X", end=" ")
+            print("A", end=" ")
         elif board[i] == 2:
             print("O", end=" ")
     print()
